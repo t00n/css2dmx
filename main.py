b1020c01d8268f789c14d6fdebc4b96563a5223d
t00n <kelkununtel@hotmail.com>
2017-12-25 03:14:27 +0100

    [add] transitions

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from itertools import chain
+from datetime import datetime
+from time import sleep
 
 from hardware import parse_hw_file
 from tree import parse_tree_file, walk_tree
 from css import parse_css_file, get_css_style
 
 
-def rgb_to_dmx(style, hw):
-    rgb = [int(x.strip()) for x in style.split("rgb(")[1].split(")")[0].split(",")]
-    return list(zip(hw, rgb))
+def parse_rgb(rgb):
+    return [int(x.strip()) for x in rgb.split("rgb(")[1].split(")")[0].split(",")]
+
+
+def color_to_dmx(color, hw):
+    return list(zip(hw, color))
+
+
+def transition_to_dmx(style):
+    pass
 
 
 def apply_style(tree, css):
@@ -17,22 +26,66 @@ def apply_style(tree, css):
             node['style'][style.name] = style.value
 
 
-def compute_dmx(tree, hw):
+def compute_transitions(style):
+    transitions = {}
+    for s in style:
+        if s == "transition":
+            prop, duration = style[s].split(" ")
+            transitions[prop] = float(duration[:-1])
+    return transitions
+
+
+def compute_dmx(tree, hw, t):
     dmx = []
     for id, node in walk_tree(tree):
         if id in hw:
+            transitions = compute_transitions(node['style'])
             for style in node['style']:
                 if style in hw[id]:
                     if style == "color":
-                        dmx.extend(rgb_to_dmx(node['style'][style], hw[id][style]))
+                        color = parse_rgb(node['style'][style])
+                        if style in transitions:
+                            ratio = t / transitions[style]
+                            color = [max(0, min(255, int(ratio * x))) for x in color]
+                        dmx.extend(list(zip(hw[id][style], color)))
     return sorted(dmx, key=lambda x: x[0])
+
+
+def trange(start=None, end=None, interval=1):
+    now = datetime.now().timestamp()
+    if start is None:
+        start = now
+    else:
+        start = start.timestamp()
+    if end is not None:
+        end = end.timestamp()
+    if start > now:
+        sleep(start - now)
+    now = start
+    i = 1
+    while end is None or now < end:
+        yield datetime.now()
+        now = datetime.now().timestamp()
+        target = start + i * interval
+        if target > now:
+            sleep(target - now)
+            now = target
+        i += 1
+
+
+def write(state):
+    print(state)
 
 
 def run(hw, tree, css):
     apply_style(tree, css)
-    print(tree)
-    dmx = compute_dmx(tree, hw)
-    print(dmx)
+    old_state = None
+    now = datetime.now()
+    for t in trange(interval=0.1):
+        state = compute_dmx(tree, hw, t.timestamp() - now.timestamp())
+        if state != old_state:
+            old_state = state
+            write(state)
 
 if __name__ == '__main__':
     import sys
