712487c82d4da28d2671336eae91ff00c9c70135
t00n <kelkununtel@hotmail.com>
2017-12-25 21:54:41 +0100

    [ref] utils

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-import math
 
 from hardware import parse_hw_file
 from tree import parse_tree_file
 from css import parse_css_file, parse_rgb, parse_transitions
-from utils import trange
+from utils import trange, compute_cubic_bezier, de_casteljau
 
 
 def apply_style(tree, css):
@@ -13,64 +12,6 @@ def apply_style(tree, css):
             for node in tree.select(selector.selectorText):
                 for prop in rule.style:
                     node.add_style(prop.name, prop.value)
-
-
-def lerp(t, a, b):
-    return t * b + (1 - t) * a
-
-
-def lerpP(t, zero, one):
-    return lerp(t, zero[0], one[0]), lerp(t, zero[1], one[1])
-
-
-def de_casteljau(t, coefs):
-    if len(coefs) == 1:
-        return coefs[0]
-    else:
-        return de_casteljau(t, [lerpP(t, x, y) for x, y in zip(coefs[:-1], coefs[1:])])
-
-
-def compute_roots_cubic(a, b, c, d):
-    b /= a
-    c /= a
-    d /= a
-
-    p = (3 * c - b ** 2) / 3
-    q = (2 * b * b ** 2 - 9 * b * c + 27 * d) / 27
-
-    if p == 0:
-        return [math.pow(-q, 1 / 3)]
-    elif q == 0:
-        return [math.sqrt(-p), -math.sqrt(-p)]
-    else:
-        discriminant = math.pow(q / 2, 2) + math.pow(p / 3, 3)
-        if discriminant == 0:
-            return [math.pow(q / 2, 1 / 3) - b / 3]
-        elif discriminant > 0:
-            return [math.pow(-(q / 2) + math.sqrt(discriminant), 1 / 3) - math.pow((q / 2) + math.sqrt(discriminant), 1 / 3) - b / 3]
-        else:
-            r = math.sqrt(math.pow(-(p / 3), 3))
-            phi = math.acos(-(q / (2 * math.sqrt(math.pow(-(p / 3), 3)))))
-            s = 2 * math.pow(r, 1 / 3)
-            return [
-                s * math.cos(phi / 3) - b / 3,
-                s * math.cos((phi + 2 * math.pi) / 3) - b / 3,
-                s * math.cos((phi + 4 * math.pi) / 3) - b / 3
-            ]
-
-
-def compute_cubic_bezier(p1, p2, x0):
-    p0 = -x0
-    p1 -= x0
-    p2 -= x0
-    p3 = 1 - x0
-
-    a = p3 - 3 * p2 + 3 * p1 - p0
-    b = 3 * p2 - 6 * p1 + 3 * p0
-    c = 3 * p1 - 3 * p0
-    d = p0
-    roots = compute_roots_cubic(a, b, c, d)
-    return [r for r in roots if r >= 0 and r <= 1]
 
 
 def compute_transition_function_at(transition, t):
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from time import sleep
+import math
 
 
 def trange(start=None, end=None, interval=1):
@@ -22,3 +23,61 @@ def trange(start=None, end=None, interval=1):
             sleep(target - now)
             now = target
         i += 1
+
+
+def lerp(t, a, b):
+    return t * b + (1 - t) * a
+
+
+def lerpP(t, zero, one):
+    return lerp(t, zero[0], one[0]), lerp(t, zero[1], one[1])
+
+
+def de_casteljau(t, coefs):
+    if len(coefs) == 1:
+        return coefs[0]
+    else:
+        return de_casteljau(t, [lerpP(t, x, y) for x, y in zip(coefs[:-1], coefs[1:])])
+
+
+def compute_roots_cubic(a, b, c, d):
+    b /= a
+    c /= a
+    d /= a
+
+    p = (3 * c - b ** 2) / 3
+    q = (2 * b * b ** 2 - 9 * b * c + 27 * d) / 27
+
+    if p == 0:
+        return [math.pow(-q, 1 / 3)]
+    elif q == 0:
+        return [math.sqrt(-p), -math.sqrt(-p)]
+    else:
+        discriminant = math.pow(q / 2, 2) + math.pow(p / 3, 3)
+        if discriminant == 0:
+            return [math.pow(q / 2, 1 / 3) - b / 3]
+        elif discriminant > 0:
+            return [math.pow(-(q / 2) + math.sqrt(discriminant), 1 / 3) - math.pow((q / 2) + math.sqrt(discriminant), 1 / 3) - b / 3]
+        else:
+            r = math.sqrt(math.pow(-(p / 3), 3))
+            phi = math.acos(-(q / (2 * math.sqrt(math.pow(-(p / 3), 3)))))
+            s = 2 * math.pow(r, 1 / 3)
+            return [
+                s * math.cos(phi / 3) - b / 3,
+                s * math.cos((phi + 2 * math.pi) / 3) - b / 3,
+                s * math.cos((phi + 4 * math.pi) / 3) - b / 3
+            ]
+
+
+def compute_cubic_bezier(p1, p2, x0):
+    p0 = -x0
+    p1 -= x0
+    p2 -= x0
+    p3 = 1 - x0
+
+    a = p3 - 3 * p2 + 3 * p1 - p0
+    b = 3 * p2 - 6 * p1 + 3 * p0
+    c = 3 * p1 - 3 * p0
+    d = p0
+    roots = compute_roots_cubic(a, b, c, d)
+    return [r for r in roots if r >= 0 and r <= 1]
