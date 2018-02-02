1b5d79f6f85c4bae9b8e5f3dcf4894211bf28cf7
t00n <kelkununtel@hotmail.com>
2018-02-02 17:04:58 +0100

    [add] simple animations work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import sys
 
 from hardware import parse_hw_file
 from tree import parse_tree_file
@@ -33,6 +34,7 @@ def get_bezier_coefs(function):
 def compute_transition_function_at(transition, t):
     p1, p2 = get_bezier_coefs(transition.function)
     coefs = (0, 0), p1, p2, (1, 1)
+    # stops transition after the duration is over
     x0 = min(1, t / transition.duration)
     x = compute_cubic_bezier(p1[0], p2[0], x0)[-1]
     y = de_casteljau(x, coefs)[1]
@@ -43,9 +45,42 @@ def compute_prop_with_ratio(src, target, ratio):
     return [int(src[i] + (target[i] - src[i]) * ratio) for i in range(len(src))]
 
 
-def compute_style(node, t):
+def compute_animations(animations, keyframes, t):
+    style = {}
+    for name, anim in animations.items():
+        if anim.iteration == 'infinite' or t <= anim.duration * anim.iteration:
+            # compute where we are in the animation
+            percent_t = (t % anim.duration) / anim.duration
+            # select the frame we're in
+            frames = keyframes[name].frames
+            for f in frames:
+                selector = f.selector / 100
+                if selector > percent_t:
+                    higher_selector = selector
+                    higher_properties = f.properties
+                    break
+                lower_selector = selector
+                lower_properties = f.properties
+            x0 = (percent_t - lower_selector) / (higher_selector - lower_selector)
+            p1, p2 = get_bezier_coefs(anim.function)
+            coefs = (0, 0), p1, p2, (1, 1)
+            x = compute_cubic_bezier(p1[0], p2[0], x0)[-1]
+            y = de_casteljau(x, coefs)[1]
+            for low_prop in lower_properties:
+                for high_prop in higher_properties:
+                    if low_prop.name == high_prop.name:
+                        value = compute_prop_with_ratio(low_prop.value, high_prop.value, y)
+                        style[low_prop.name] = value
+    return style
+
+
+def compute_style(node, keyframes, t):
     style = {}
     for prop in node.style:
+        if prop == 'animation':
+            props_animation = compute_animations(node.style[prop], keyframes, t)
+            for name, value in props_animation.items():
+                style[name] = value
         if 'transition' in node.style and prop in node.style['transition']:
             src = get_default_style(prop)
             target = node.style[prop]
@@ -57,11 +92,11 @@ def compute_style(node, t):
     return style
 
 
-def compute_dmx(tree, hw, t):
+def compute_dmx(tree, hw, keyframes, t):
     dmx = []
     for node in tree.walk():
         if node.id in hw:
-            style = compute_style(node, t)
+            style = compute_style(node, keyframes, t)
             for prop, val in style.items():
                 if prop in hw[node.id]:
                     dmx.extend(list(zip(hw[node.id][prop], val)))
@@ -75,10 +110,11 @@ def send_dmx(state):
 def run(hw, tree, css):
     apply_style(tree, css)
     tree.print()
+    keyframes = css.keyframes
     old_state = None
     now = datetime.now()
     for t in trange(interval=0.01):
-        state = compute_dmx(tree, hw, t.timestamp() - now.timestamp())
+        state = compute_dmx(tree, hw, keyframes, t.timestamp() - now.timestamp())
         if state != old_state:
             old_state = state
             send_dmx(state)
